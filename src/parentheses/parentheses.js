06e8dfe5d3df325c6fb80f922c240149875be7bb
Russell Smith <resmithjr88@gmail.com>
2022-09-29 09:20:55 -0600

    Completed parentheses with tests passing

--- src/parentheses/parentheses.js
+++ src/parentheses/parentheses.js
@@ -1,5 +1,21 @@
 const Stack = require("../lib/stack");
 
-const match = (expression) => {};
+const match = (expression) => {
+    let parens = new Stack()
+
+    for (let i = 0; i < expression.length; i++) {
+        if (expression[i] === "(") {
+            parens.push("(")
+        } else {
+            if (expression[i] === ")") {
+                if (parens.top) {
+                    parens.pop()
+                }
+                else return false
+            }
+        }
+    }
+    return !parens.top
+};
 
 module.exports = match;
