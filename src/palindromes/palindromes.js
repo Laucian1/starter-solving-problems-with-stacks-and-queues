8454c0dc1e331d00499930a8bc471cf4de329781
Russell Smith <resmithjr88@gmail.com>
2022-09-29 09:10:37 -0600

    Palindromes complete and passing

--- src/palindromes/palindromes.js
+++ src/palindromes/palindromes.js
@@ -2,7 +2,23 @@ const Stack = require("../lib/stack");
 
 const isPalindrome = (sentence) => {
   sentence = sentence.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
-  // Your solution here
+  let middle = Math.floor(sentence.length / 2)
+
+  let compare = new Stack()
+
+  for (let i = 0; i < middle; i++) {
+    compare.push(sentence[i])
+  }
+
+  middle += sentence.length % 2 === 0 ? 0 : 1
+
+  for ( let i = middle; limit = sentence.length, i < limit; i++) {
+      if (sentence[i] !== compare.pop()) {
+        return false
+      }
+    }
+
+    return true
 };
 
 module.exports = isPalindrome;
